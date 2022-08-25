cd3ab7d00c5443dee895d6680a5fe9774b4d8bc7
keita-makino <kemakino@ucdavis.edu>
2022-08-25 16:27:07 -0700

    changed button text color to white

--- src/components/ClearButton.tsx
+++ src/components/ClearButton.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import { Grid, Button, Typography } from '@mui/material';
 import React from 'react';
 import { useUpdate } from '../store';
@@ -9,16 +10,20 @@ export const ClearButton: React.FC<ClearButtonProps> = (
 ) => {
   const update = useUpdate();
 
+  const StyledButton = styled(Button)({
+    color: 'white !important',
+  }) as typeof Button;
+
   return (
     <Grid container>
       <Grid item container xl={6} lg={6} md={6} sm={6} xs={6}>
-        <Button
+        <StyledButton
           variant={'contained'}
           color={'primary'}
           onClick={() => update({ type: 'CLEAR_INPUT' })}
         >
           <Typography variant={'button'}>Reset Input(s)</Typography>
-        </Button>
+        </StyledButton>
       </Grid>
     </Grid>
   );
